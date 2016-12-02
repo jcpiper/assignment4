693878be742393a353e4de9a05bca08de2e8c0aa
Justin Piper <jpiper4198@gmail.com>
2016-12-01 23:51:44 -0500

    functionality and styling good to go

--- assignment4.js
+++ assignment4.js
@@ -14,4 +14,34 @@
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+  $('.flexsearch-input').keyup(function() {
+    var search = $('.flexsearch-input').val();
+    if (search == '') {
+      return;
+    }
+    $('#inputSuggestions').empty();
+    $.ajax({
+      type: "get",
+      url: "http://www.mattbowytz.com/simple_api.json",
+      data: {
+        data: "all"
+      },
+      success: function (data) {
+        // console.log(data);
+        $.each(data.data, function(array, subarr){
+          $.each(subarr, function(index, option){
+            // console.log(option);
+            if(option.substring(0, search.length).toLowerCase() == search) {
+              var result = "<li><a href=https://www.google.com/search?q=" + option + ">" + option + "</li>";
+              $('#inputSuggestions').append(result);
+
+              console.log(option);
+            }
+          });
+        });
+      }
+      // }).done(function(data) {
+      //   console.log(data);
+    });
+  });
 })();
